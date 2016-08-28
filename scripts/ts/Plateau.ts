9b868102f638c895445a8b5f155f4f6daed9ccce
Pranav Joshi <joshpranav@gmail.com>
2016-08-28 14:09:59 +1000

    rover on plateau

--- scripts/ts/Plateau.ts
+++ scripts/ts/Plateau.ts
@@ -1,13 +1,28 @@
+import {Rover} from "./Rover";
+import {Constants} from "./Constants";
+
 class Plateau {
     constructor (xlength : number, ylength : number) {
         this.xLength = xlength;
         this.yLength = ylength;
     }
 
+    public Rover : Rover;
     public xLength : number;
     public yLength : number;
 
-    public IsValidPosition(x: number, y: number) : boolean {
+    public InitRover(rover : Rover) {
+        this.Rover = rover;
+        if(!this.IsValidRoverPosition(rover.RoverPosition.X, rover.RoverPosition.Y)) {
+            throw Constants.InvalidRoverPosition;
+        }
+    }
+
+    public MoveRover(commands : string[]) {
+        
+    }
+
+    public IsValidRoverPosition(x: number, y: number) : boolean {
         if(x > this.xLength || y >  this.yLength) {
             return false;
         }
--- scripts/ts/Rover.ts
+++ scripts/ts/Rover.ts
@@ -1,6 +1,5 @@
 import {Coordinates} from "./Coordinates";
 import {IRoverDirection} from "./IRoverDirection";
-import {Constants} from "./Constants";
 import {RoverDirectionFactory} from "./RoverDirectionFactory";
 import {Plateau} from  "./Plateau";
 
@@ -12,25 +11,13 @@ class Rover {
 
     public RoverPosition : Coordinates;
     public RoverDirection : IRoverDirection;
-
-    public Execute(commands : string[]) : string {
-        for(let cmd in commands)
-        {
-            // if(CommandsHelper.IsValidCommand(cmd) == false)
-            // {
-            //     throw Constants.InValidCommandMessage;
-            // }
-        }
-        return this.GetCurrentStateDescription();
-    }
-
+    
     private GetCurrentStateDescription() : string {
         let direction = this.RoverDirection.DirectionIdentifier;
         let position = `(${this.RoverPosition.X},${this.RoverPosition.Y})`;
 
         return `Rover is at ${position} facing ${direction}.`;
     }
-
 }
 
 export {Rover};
