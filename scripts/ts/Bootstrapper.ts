9e6d3be267712dfef38691ed54822afe34f8e914
Pranav Joshi <joshpranav@gmail.com>
2016-09-01 21:13:25 +1000

    bootstrapper

--- scripts/ts/Bootstrapper.ts
+++ scripts/ts/Bootstrapper.ts
@@ -9,8 +9,8 @@ import {Position} from "./entities/Position";
 let plateau = new Plateau(20,20,new PlateauValidator());
 let position = new Position(5,5);
 let rover = new Rover(plateau, position, "N", new RoverValidator());
-
 let commands = ["L","L","M","R","M","M"];
+
 let state = rover.Execute(commands);
 
 console.log(state);
